4bee059664c69b778c3783cccc45bd7fbdcea8f5
Ganna Rusnak <grusnak@cccis.com>
2023-07-11 20:21:41 +0300

    UC#12 Create documentation

--- CommentEndpoint.java
+++ CommentEndpoint.java
@@ -1,13 +1,13 @@
 package com.softserve.taf.services.placeholder.endpoints;
 
-import io.restassured.response.ValidatableResponse;
-import io.restassured.specification.RequestSpecification;
-import java.util.List;
-import org.apache.logging.log4j.LogManager;
-import org.apache.logging.log4j.Logger;
 import com.softserve.taf.models.enums.HttpStatus;
 import com.softserve.taf.models.placeholder.comment.CommentDto;
 import com.softserve.taf.services.common.AbstractWebEndpoint;
+import io.restassured.response.ValidatableResponse;
+import io.restassured.specification.RequestSpecification;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+import java.util.List;
 
 public class CommentEndpoint extends AbstractWebEndpoint {
 
@@ -19,59 +19,109 @@ public class CommentEndpoint extends AbstractWebEndpoint {
         super(specification);
     }
 
+    /**
+     * Create a comment
+     *
+     * @param commentDto {@link CommentDto}
+     * @return new {@link CommentDto}
+     */
     public CommentDto create(CommentDto commentDto) {
         return create(commentDto, HttpStatus.CREATED)
-            .extract().as(CommentDto.class);
+                .extract().as(CommentDto.class);
     }
 
+    /**
+     * Create a comment with specific status code
+     *
+     * @param commentDto {@link CommentDto}
+     * @param status     {@link HttpStatus}
+     * @return {@link ValidatableResponse}
+     */
     public ValidatableResponse create(CommentDto commentDto, HttpStatus status) {
         LOGGER.info("Create new Comment");
         return post(
-            this.specification,
-            COMMENTS_END,
-            commentDto)
-            .statusCode(status.getCode());
+                this.specification,
+                COMMENTS_END,
+                commentDto)
+                .statusCode(status.getCode());
     }
 
+    /**
+     * Update an exicting comment
+     *
+     * @param id         integer
+     * @param commentDto {@link CommentDto}
+     * @return updated {@link CommentDto}
+     */
     public CommentDto update(int id, CommentDto commentDto) {
         return update(commentDto, id, HttpStatus.OK)
-            .extract().as(CommentDto.class);
+                .extract().as(CommentDto.class);
     }
 
+    /**
+     * Update an exicting comment with specific status code
+     *
+     * @param commentDto {@link CommentDto}
+     * @param id         integer
+     * @param status     {@link HttpStatus}
+     * @return {@link ValidatableResponse}
+     */
     public ValidatableResponse update(CommentDto commentDto, int id, HttpStatus status) {
         LOGGER.info("Update Comment by id [{}]", id);
         return put(
-            this.specification,
-            COMMENTS_RESOURCE_END,
-            commentDto,
-            id)
-            .statusCode(status.getCode());
+                this.specification,
+                COMMENTS_RESOURCE_END,
+                commentDto,
+                id)
+                .statusCode(status.getCode());
     }
 
+    /**
+     * Get comment by id
+     *
+     * @param id integer
+     * @return {@link CommentDto}
+     */
     public CommentDto getById(int id) {
         return getById(id, HttpStatus.OK)
-            .extract().as(CommentDto.class);
+                .extract().as(CommentDto.class);
     }
 
+    /**
+     * Get comment by id with specific status code
+     *
+     * @param id     integer
+     * @param status {@link HttpStatus}
+     * @return {@link ValidatableResponse}
+     */
     public ValidatableResponse getById(int id, HttpStatus status) {
         LOGGER.info("Get Comment by id [{}]", id);
         return get(
-            this.specification,
-            COMMENTS_RESOURCE_END,
-            String.valueOf(id))
-            .statusCode(status.getCode());
+                this.specification,
+                COMMENTS_RESOURCE_END,
+                String.valueOf(id))
+                .statusCode(status.getCode());
     }
 
+    /**
+     * Get all comments
+     *
+     * @return List of {@link CommentDto}
+     */
     public List<CommentDto> getAll() {
         return List.of(getAll(HttpStatus.OK).extract().as(CommentDto[].class));
     }
 
+    /**
+     * Get all comments with specific status code
+     *
+     * @param status {@link HttpStatus}
+     * @return {@link ValidatableResponse}
+     */
     public ValidatableResponse getAll(HttpStatus status) {
         LOGGER.info("Get all Comments");
         ValidatableResponse response = get(this.specification, COMMENTS_END);
         response.statusCode(status.getCode());
         return response;
     }
-
-
 }
--- UserEndpoint.java
+++ UserEndpoint.java
@@ -1,13 +1,13 @@
 package com.softserve.taf.services.placeholder.endpoints;
 
-import io.restassured.response.ValidatableResponse;
-import io.restassured.specification.RequestSpecification;
-import java.util.List;
-import org.apache.logging.log4j.LogManager;
-import org.apache.logging.log4j.Logger;
 import com.softserve.taf.models.enums.HttpStatus;
 import com.softserve.taf.models.placeholder.user.UserDto;
 import com.softserve.taf.services.common.AbstractWebEndpoint;
+import io.restassured.response.ValidatableResponse;
+import io.restassured.specification.RequestSpecification;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+import java.util.List;
 
 public class UserEndpoint extends AbstractWebEndpoint {
 
@@ -19,58 +19,109 @@ public class UserEndpoint extends AbstractWebEndpoint {
         super(specification);
     }
 
+    /**
+     * Create a user
+     *
+     * @param userDto {@link @UserDto}
+     * @return
+     */
     public UserDto create(UserDto userDto) {
         return create(userDto, HttpStatus.CREATED)
-            .extract().as(UserDto.class);
+                .extract().as(UserDto.class);
     }
 
+    /**
+     * Create a user with specific status code
+     *
+     * @param userDto {@link UserDto}
+     * @param status  {@link HttpStatus}
+     * @return {@link ValidatableResponse}
+     */
     public ValidatableResponse create(UserDto userDto, HttpStatus status) {
         LOGGER.info("Create new User");
         return post(
-            this.specification,
-            USERS_END,
-            userDto)
-            .statusCode(status.getCode());
+                this.specification,
+                USERS_END,
+                userDto)
+                .statusCode(status.getCode());
     }
 
+    /**
+     * Update user
+     *
+     * @param id      integer
+     * @param userDto {@link UserDto}
+     * @return updated {@link UserDto}
+     */
     public UserDto update(int id, UserDto userDto) {
         return update(userDto, id, HttpStatus.OK)
-            .extract().as(UserDto.class);
+                .extract().as(UserDto.class);
     }
 
+    /**
+     * Update user with specific status code
+     *
+     * @param userDto {@link UserDto}
+     * @param id      integer
+     * @param status  {@link HttpStatus}
+     * @return {@link ValidatableResponse}
+     */
     public ValidatableResponse update(UserDto userDto, int id, HttpStatus status) {
         LOGGER.info("Update User by id [{}]", id);
         return put(
-            this.specification,
-            USERS_RESOURCE_END,
-            userDto,
-            id)
-            .statusCode(status.getCode());
+                this.specification,
+                USERS_RESOURCE_END,
+                userDto,
+                id)
+                .statusCode(status.getCode());
     }
 
+    /**
+     * Get user by id
+     *
+     * @param id String
+     * @return {@link UserDto}
+     */
     public UserDto getById(String id) {
         return getById(id, HttpStatus.OK)
-            .extract().as(UserDto.class);
+                .extract().as(UserDto.class);
     }
 
+    /**
+     * Get user by id with specific status code
+     *
+     * @param id     String
+     * @param status {@link HttpStatus}
+     * @return {@link ValidatableResponse}
+     */
     public ValidatableResponse getById(String id, HttpStatus status) {
         LOGGER.info("Get User by id [{}]", id);
         return get(
-            this.specification,
-            USERS_RESOURCE_END,
-            id)
-            .statusCode(status.getCode());
+                this.specification,
+                USERS_RESOURCE_END,
+                id)
+                .statusCode(status.getCode());
     }
 
+    /**
+     * Get all users
+     *
+     * @return List of {@link UserDto}
+     */
     public List<UserDto> getAll() {
         return List.of(getAll(HttpStatus.OK).extract().as(UserDto[].class));
     }
 
+    /**
+     * Get aa users with specific status code
+     *
+     * @param status {@link HttpStatus}
+     * @return {@link ValidatableResponse}
+     */
     public ValidatableResponse getAll(HttpStatus status) {
         LOGGER.info("Get all Users");
         ValidatableResponse response = get(this.specification, USERS_END);
         response.statusCode(status.getCode());
         return response;
     }
-
 }
